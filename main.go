6a6cca9ee05e1a754d894a7768812a369a1e53ff
Kenneth Shaw <kenshaw@gmail.com>
2022-07-27 05:27:41 +0700

    Cleaning up proxy

--- main.go
+++ main.go
@@ -27,12 +27,75 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var (
-	flagListen  = flag.String("l", "localhost:9223", "listen address")
-	flagRemote  = flag.String("r", "localhost:9222", "remote address")
-	flagNoLog   = flag.Bool("n", false, "disable logging to file")
-	flagLogMask = flag.String("log", "logs/cdp-%s.log", "log file mask")
-)
+func main() {
+	listen := flag.String("l", "localhost:9223", "listen address")
+	remote := flag.String("r", "localhost:9222", "remote address")
+	noLog := flag.Bool("n", false, "disable logging to file")
+	logMask := flag.String("log", "logs/cdp-%s.log", "log file mask")
+	flag.Parse()
+	if err := run(context.Background(), *listen, *remote, *noLog, *logMask); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, listen, remote string, noLog bool, logMask string) error {
+	mux := http.NewServeMux()
+	simplep := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: "http",
+		Host:   remote,
+	})
+	mux.Handle("/json", simplep)
+	mux.Handle("/", simplep)
+	mux.HandleFunc("/devtools/", func(res http.ResponseWriter, req *http.Request) {
+		id := path.Base(req.URL.Path)
+		f, logger := createLog(noLog, logMask, id)
+		if f != nil {
+			defer f.Close()
+		}
+		logger.Printf("---------- connection from %s ----------", req.RemoteAddr)
+		ver, err := checkVersion(ctx, remote)
+		if err != nil {
+			msg := fmt.Sprintf("version error, got: %v", err)
+			logger.Println(msg)
+			http.Error(res, msg, http.StatusInternalServerError)
+			return
+		}
+		logger.Printf("endpoint %s reported: %s", remote, string(ver))
+		endpoint := "ws://" + remote + path.Join(path.Dir(req.URL.Path), id)
+		// connect outgoing websocket
+		logger.Printf("connecting to %s", endpoint)
+		out, pres, err := wsDialer.Dial(endpoint, nil)
+		if err != nil {
+			msg := fmt.Sprintf("could not connect to %s, got: %v", endpoint, err)
+			logger.Println(msg)
+			http.Error(res, msg, http.StatusInternalServerError)
+			return
+		}
+		defer pres.Body.Close()
+		defer out.Close()
+		logger.Printf("connected to %s", endpoint)
+		// connect incoming websocket
+		logger.Printf("upgrading connection on %s", req.RemoteAddr)
+		in, err := wsUpgrader.Upgrade(res, req, nil)
+		if err != nil {
+			msg := fmt.Sprintf("could not upgrade websocket from %s, got: %v", req.RemoteAddr, err)
+			logger.Println(msg)
+			http.Error(res, msg, http.StatusInternalServerError)
+			return
+		}
+		defer in.Close()
+		logger.Printf("upgraded connection on %s", req.RemoteAddr)
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		errc := make(chan error, 1)
+		go proxyWS(ctx, logger, "<-", in, out, errc)
+		go proxyWS(ctx, logger, "->", out, in, errc)
+		<-errc
+		logger.Printf("---------- closing %s ----------", req.RemoteAddr)
+	})
+	return http.ListenAndServe(listen, mux)
+}
 
 const (
 	incomingBufferSize = 10 * 1024 * 1024
@@ -52,79 +115,13 @@ var wsDialer = &websocket.Dialer{
 	WriteBufferSize: incomingBufferSize,
 }
 
-func main() {
-	flag.Parse()
-
-	mux := http.NewServeMux()
-	simplep := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: "http", Host: *flagRemote})
-	mux.Handle("/json", simplep)
-	mux.Handle("/", simplep)
-	mux.HandleFunc("/devtools/", func(res http.ResponseWriter, req *http.Request) {
-		id := path.Base(req.URL.Path)
-		f, logger := createLog(id)
-		if f != nil {
-			defer f.Close()
-		}
-		logger.Printf("---------- connection from %s ----------", req.RemoteAddr)
-
-		ver, err := checkVersion()
-		if err != nil {
-			msg := fmt.Sprintf("version error, got: %v", err)
-			logger.Println(msg)
-			http.Error(res, msg, http.StatusInternalServerError)
-			return
-		}
-		logger.Printf("endpoint %s reported: %s", *flagRemote, string(ver))
-
-		endpoint := "ws://" + *flagRemote + path.Join(path.Dir(req.URL.Path), id)
-
-		// connect outgoing websocket
-		logger.Printf("connecting to %s", endpoint)
-		out, pres, err := wsDialer.Dial(endpoint, nil)
-		if err != nil {
-			msg := fmt.Sprintf("could not connect to %s, got: %v", endpoint, err)
-			logger.Println(msg)
-			http.Error(res, msg, http.StatusInternalServerError)
-			return
-		}
-		defer pres.Body.Close()
-		defer out.Close()
-
-		logger.Printf("connected to %s", endpoint)
-
-		// connect incoming websocket
-		logger.Printf("upgrading connection on %s", req.RemoteAddr)
-		in, err := wsUpgrader.Upgrade(res, req, nil)
-		if err != nil {
-			msg := fmt.Sprintf("could not upgrade websocket from %s, got: %v", req.RemoteAddr, err)
-			logger.Println(msg)
-			http.Error(res, msg, http.StatusInternalServerError)
-			return
-		}
-		defer in.Close()
-		logger.Printf("upgraded connection on %s", req.RemoteAddr)
-
-		ctxt, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		errc := make(chan error, 1)
-		go proxyWS(ctxt, logger, "<-", in, out, errc)
-		go proxyWS(ctxt, logger, "->", out, in, errc)
-		<-errc
-		logger.Printf("---------- closing %s ----------", req.RemoteAddr)
-	})
-
-	log.Fatal(http.ListenAndServe(*flagListen, mux))
-}
-
 // proxyWS proxies in and out messages for a websocket connection, logging the
 // message to the logger with the passed prefix. Any error encountered will be
 // sent to errc.
-func proxyWS(ctxt context.Context, logger *log.Logger, prefix string, in, out *websocket.Conn, errc chan error) {
+func proxyWS(ctx context.Context, logger *log.Logger, prefix string, in, out *websocket.Conn, errc chan error) {
 	var mt int
 	var buf []byte
 	var err error
-
 	for {
 		select {
 		default:
@@ -133,16 +130,13 @@ func proxyWS(ctxt context.Context, logger *log.Logger, prefix string, in, out *w
 				errc <- err
 				return
 			}
-
 			logger.Println(prefix, string(buf))
-
 			err = out.WriteMessage(mt, buf)
 			if err != nil {
 				errc <- err
 				return
 			}
-
-		case <-ctxt.Done():
+		case <-ctx.Done():
 			return
 		}
 	}
@@ -150,51 +144,44 @@ func proxyWS(ctxt context.Context, logger *log.Logger, prefix string, in, out *w
 
 // checkVersion retrieves the version information for the remote endpoint, and
 // formats it appropriately.
-func checkVersion() ([]byte, error) {
-	cl := &http.Client{}
-	req, err := http.NewRequest("GET", "http://"+*flagRemote+"/json/version", nil)
+func checkVersion(ctx context.Context, remote string) ([]byte, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://"+remote+"/json/version", nil)
 	if err != nil {
 		return nil, err
 	}
-
+	cl := &http.Client{}
 	res, err := cl.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
-
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
-
 	var v map[string]string
-	err = json.Unmarshal(body, &v)
-	if err != nil {
-		return nil, fmt.Errorf("expected json result")
+	if err := json.Unmarshal(body, &v); err != nil {
+		return nil, fmt.Errorf("expected json result: %w", err)
 	}
-
 	return body, nil
 }
 
-var (
-	idCleanRE = regexp.MustCompile(`[^a-zA-Z0-9_\-\.]`)
-)
-
 // createLog creates a log for the specified id based on flags.
-func createLog(id string) (io.Closer, *log.Logger) {
+func createLog(noLog bool, logMask, id string) (io.Closer, *log.Logger) {
 	var f io.Closer
 	var w io.Writer = os.Stdout
-	if !*flagNoLog && *flagLogMask != "" {
-		filename := *flagLogMask
-		if strings.Contains(*flagLogMask, "%s") {
-			filename = fmt.Sprintf(*flagLogMask, idCleanRE.ReplaceAllString(id, ""))
+	if !noLog && logMask != "" {
+		filename := logMask
+		if strings.Contains(logMask, "%s") {
+			filename = fmt.Sprintf(logMask, cleanRE.ReplaceAllString(id, ""))
 		}
-		l, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		l, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o644)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 		f, w = l, io.MultiWriter(os.Stdout, l)
 	}
 	return f, log.New(w, "", log.LstdFlags)
 }
+
+var cleanRE = regexp.MustCompile(`[^a-zA-Z0-9_\-\.]`)
